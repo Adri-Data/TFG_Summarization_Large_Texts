4d306bb5b9c15b4ef1ac641f8b007103179ec6db
Adri-Data <163586288+Adri-Data@users.noreply.github.com>
2024-03-17 16:08:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,46 +16,72 @@ from summarizer import Summarizer
 nltk.download('wordnet')
 
 def initialize_model_and_tokenizer(model_name):
+
+    # Cargar el tokenizador y el modelo
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    if model_name in ["tuner007/pegasus_summarizer", "microsoft/prophetnet-large-uncased"]:
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    if (model_name=="tuner007/pegasus_summarizer" or model_name=="microsoft/prophetnet-large-uncased"):
+      model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     else:
-        model = AutoModelWithLMHead.from_pretrained(model_name)
+      model = AutoModelWithLMHead.from_pretrained(model_name)
+
+    # Mover el modelo a la GPU si está disponible
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
+
     return model, tokenizer, device
 
+def sumary_final(inputs):
+  summary_ids = model.generate(inputs, max_length=150, min_length=100, length_penalty=0.1, num_beams=16, no_repeat_ngram_size=4)
+  return summary_ids
+
 def transcribe_and_translate(url, source_lang='es', target_lang='en', device='cuda'):
+    # Crear un objeto YouTube
     yt = YouTube(url)
+    # Obtener el stream de audio de mayor calidad
     audio_stream = yt.streams.get_audio_only()
+    # Descargar el audio
     audio_file = audio_stream.download()
+    # Transcribir el audio usando whisperx
     model = whisperx.load_model("large-v2", device, compute_type="float16")
     audio = whisperx.load_audio(audio_file)
     result = model.transcribe(audio, batch_size=16)
+    # Alinear el texto con el audio
     model_a, metadata = whisperx.load_align_model(language_code=source_lang, device=device)
     result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+    # Traducir el texto al inglés usando MarianMT
     modelo_nombre = f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}'
     modelo = MarianMTModel.from_pretrained(modelo_nombre).to(device)
     tokenizador = MarianTokenizer.from_pretrained(modelo_nombre)
+
     def traducir_segmento(segmento):
         id, texto = segmento
+        # Codificar el texto y generar la traducción
         texto_tokenizado = tokenizador.prepare_seq2seq_batch([texto], return_tensors='pt').to(device)
         traduccion_ids = modelo.generate(**texto_tokenizado)
         traduccion = tokenizador.decode(traduccion_ids[0], skip_special_tokens=True)
         return id, traduccion
+
+    # Codificar el texto y generar la traducción
     with ThreadPoolExecutor() as executor:
         segmentos = [(i, diccionario['text']) for i, diccionario in enumerate(result["segments"])]
         traducciones = list(executor.map(traducir_segmento, segmentos))
-    texto_traducido = " ".join(traduccion for id, traduccion in sorted(traducciones))
+        # Ordenar las traducciones por id y juntarlas
+        texto_traducido = " ".join(traduccion for id, traduccion in sorted(traducciones))
+        # Devolver el texto traducido
     return texto_traducido
 
+
 def divide_texto(texto, tokenizer, longitud_max=512):
+
     nltk.download('punkt', quiet=True)
+
+    # Divide el texto en oraciones utilizando NLTK
     oraciones = nltk.tokenize.sent_tokenize(texto)
+
     trozos = []
     trozo_actual = ''
     for i, parrafo in enumerate(oraciones):
-        if len(tokenizer.encode(trozo_actual + ' \n ' + parrafo)) <= longitud_max:
+        if len(tokenizer.encode(trozo_actual + '\n' + parrafo)) <= longitud_max:
             trozo_actual += '\n' + parrafo
         else:
             trozos.append((i, trozo_actual))
@@ -65,19 +91,39 @@ def divide_texto(texto, tokenizer, longitud_max=512):
 
 def resumir_texto(trozo, model, tokenizer, device):
     id, texto = trozo
+    # Codificar el texto y generar el resumen
     inputs = tokenizer.encode("summarize: " + texto, return_tensors='pt', max_length=512, truncation=True, padding='longest').to(device)
     summary_ids = model.generate(inputs, max_length=100, min_length=20, length_penalty=3, num_beams=4, early_stopping=True, no_repeat_ngram_size=3)
     resumen = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return id, resumen
 
+def resumir_texto_final(trozo, model, tokenizer, device):
+    id, texto = trozo
+    # Codificar el texto y generar el resumen
+    inputs = tokenizer.encode("summarize: " + texto, return_tensors='pt', truncation=True, padding='longest').to(device)
+    summary_ids = sumary_final(inputs)
+    resumen = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-def resumir_texto_paralelo(texto, model, tokenizer, device, max_length):
+    return id, resumen
+
+
+def resumir_texto_paralelo(texto, model, tokenizer, device, max_length, print_option="no"):
+    # Dividir el texto en trozos
     if len(tokenizer.encode(texto)) <= max_length:
-        return resumir_texto((None, texto), model, tokenizer, device)[1]
+        return resumir_texto_final((None, texto), model, tokenizer, device)[1]
+
     trozos = divide_texto(texto, tokenizer, max_length)
+
+    # Si el número de tokens es menor que max_length, llama a resumir_texto_final
+
+
     resumenes = {}
+
+    # Crea un pool de trabajadores para ejecutar resumir_texto en paralelo
     with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Mapea resumir_texto a cada trozo de texto
         future_to_trozo = {executor.submit(resumir_texto, trozo, model, tokenizer, device): trozo for trozo in trozos}
+
         for i, future in enumerate(concurrent.futures.as_completed(future_to_trozo)):
             trozo = future_to_trozo[future]
             try:
@@ -86,13 +132,26 @@ def resumir_texto_paralelo(texto, model, tokenizer, device, max_length):
                 print(f'El trozo {trozo} generó una excepción: {exc}')
             else:
                 resumenes[id] = resumen
+                porcentaje_completado = ((i + 1) / len(trozos)) * 100
+                if (print_option!="no"):
+                  print(f'Proceso completado: {i + 1}. Porcentaje completado: {porcentaje_completado}%')
+                  print(f'Resumen: {resumen}')
+
+    # Ordenar los resúmenes por id y juntarlos
     texto_resumen = ' '.join(resumen for id, resumen in sorted(resumenes.items()))
-    texto_resumen = resumir_texto_paralelo(texto_resumen, model, tokenizer, device, max_length)
+
+    # Si los tokens de texto_resumen son más que max_length, llama a resumir_texto_paralelo de forma recursiva
+    texto_resumen = resumir_texto_paralelo(texto_resumen, model, tokenizer, device, max_length, print_option)
+
     return texto_resumen
 
 def generar_resumen_extractivo(texto, ratio, algoritmo="pagerank"):
+    # Inicializa el modelo
     model = Summarizer()
-    resumen = model(body=texto, ratio=ratio)  # Usar 'body' en lugar de pasar directamente el texto
+
+    # Genera el resumen
+    resumen = model(texto, ratio=ratio, algorithm=algoritmo)
+
     return resumen
 
 def start_transcription(url, source_lang):
@@ -179,7 +238,7 @@ if 'transcription_done' in st.session_state and st.session_state.transcription_d
         st.write(f"\n Generated Summary with pipeline and extractive summarization: {traductor(summary_pipeline_extracted)}")
     
         st.write("_________________________________________________________________\n\n")
-
+        '''
         # Sistema de feedback
         summary_options = [summary_original, summary_pipeline, summary_original_extracted, summary_pipeline_extracted]
         summary_labels = ["Resumen original", "Resumen con pipeline", "Resumen con resumen extractivo", "Resumen con pipeline y resumen extractivo"]
@@ -192,4 +251,4 @@ if 'transcription_done' in st.session_state and st.session_state.transcription_d
             # Almacenamiento de votos
             with open('votes.csv', 'a', newline='') as f:
                 writer = csv.writer(f)
-                writer.writerow([summary_labels[user_vote]])
+                writer.writerow([summary_labels[user_vote]])'''
